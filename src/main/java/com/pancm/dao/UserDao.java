0a2816476288d75477feb49df72c3bcb9e6d369f
master-cai-code <61615652+master-cai-code@users.noreply.github.com>
2020-03-06 13:13:10 +0800

    Update UserDao.java

--- src/main/java/com/pancm/dao/UserDao.java
+++ src/main/java/com/pancm/dao/UserDao.java
@@ -11,13 +11,7 @@ import org.apache.ibatis.annotations.Update;
 import com.pancm.pojo.User;
 
 /**
- * 
-* Title: UserDao
-* Description:
-* 用户数据接口 
-* Version:1.0.0  
-* @author pancm
-* @date 2018年1月9日
+ 
  */
 @Mapper
 public interface UserDao {
